a17c2b68afa4ad2a57c5fa36b3a624e1421e517b
thinkofmia <thinkofmia@gmail.com>
2021-03-31 12:41:45 +0800

    Added services

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -14,6 +14,8 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +33,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatButtonModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
