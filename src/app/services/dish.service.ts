88042ffd196b3de82f38e5762f568c5581b58039
thinkofmia <thinkofmia@gmail.com>
2021-04-07 11:17:49 +0800

    Convert to observerable

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
 @Injectable({
@@ -11,30 +11,33 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]>{
-    /*return new Promise(resolve => {
+  getDishes(): Observable<Dish[]>{
+  /*getDishes(): Promise<Dish[]>{
+    return new Promise(resolve => {
       // Simulate server latency with 2 seconds delay
       setTimeout(() => resolve(DISHES), 2000);
-    });*/
-
-    return of(DISHES).pipe(delay(2000)).toPromise();
+    });
+  */
+    return of(DISHES).pipe(delay(2000));
   }
 
-  getDish(id: string): Promise<Dish> {
+  getDish(id: string): Observable<Dish> {
+  //getDish(id: string): Promise<Dish> {
     /*return new Promise(resolve => {
       // Simulate server latency with 2 seconds delay
       setTimeout(() => resolve(DISHES.filter((dish)=> ( dish.id == id))[0]), 2000);
     });*/
 
-    return of(DISHES.filter((dish)=> ( dish.id == id))[0]).pipe(delay(2000)).toPromise();
+    return of(DISHES.filter((dish)=> ( dish.id == id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedDish(): Promise<Dish> {
+  getFeaturedDish(): Observable<Dish> {
+  //getFeaturedDish(): Promise<Dish> {
     /*return new Promise(resolve => {
       // Simulate server latency with 2 seconds delay
       setTimeout(() => resolve(DISHES.filter((dish)=> dish.featured)[0]), 2000);
     });*/
-    return of(DISHES.filter((dish)=> dish.featured)[0]).pipe(delay(2000)).toPromise();
+    return of(DISHES.filter((dish)=> dish.featured)[0]).pipe(delay(2000));
     
   }
 }
