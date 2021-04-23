f88ad11565ead88e70f6cce9531ffc1d90996d10
thinkofmia <thinkofmia@gmail.com>
2021-04-23 11:34:37 +0800

    Updated valid response for feedback

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -11,22 +11,21 @@ import { ProcessHTTPMsgService } from './process-httpmsg.service';
 })
 export class FeedbackService {
 
-  constructor(private http:HttpClient,
+  constructor(private http: HttpClient,
     private processHTTPMsgService: ProcessHTTPMsgService) { }
 
   getFeedback(): Observable<Feedback>{
       return this.http.get<Feedback>(baseURL + 'feedback')
         .pipe(catchError(this.processHTTPMsgService.handleError));
     }
-
+    
   submitFeedback(feedback: Feedback): Observable<Feedback> {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+        'Content-Type':  'application/json'
       })
     };
-
-    return this.http.put<Feedback>(baseURL + 'feedback' , feedback, httpOptions)
-      .pipe(catchError(this.processHTTPMsgService.handleError));
+    return this.http.post<Feedback>(baseURL + 'feedback/', feedback, httpOptions)
+    .pipe(catchError(this.processHTTPMsgService.handleError));
   }
 }
